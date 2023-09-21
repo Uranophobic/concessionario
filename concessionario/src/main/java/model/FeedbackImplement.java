ab250113c94b2932ae7002515e64da55375693e4
Alessia Crispo <alessia.crispo24@gmail.com>
2023-09-21 10:41:29 +0200

    Feedback implement add

--- concessionario/src/main/java/model/FeedbackImplement.java
+++ concessionario/src/main/java/model/FeedbackImplement.java
@@ -3,6 +3,7 @@ package model;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -33,33 +34,73 @@ public class FeedbackImplement implements FeedbackModel {
 
 	@Override
 	public void doUpdate(Feedback feedback) throws SQLException {
-//DA TOGLIERE PERCHè NON PUò ESSERE MODIFICATO UN FEEDBACK
+		//DA TOGLIERE PERCHè NON PUò ESSERE MODIFICATO UN FEEDBACK
 	}
 
 	@Override
 	public void doDelete(Feedback feedback) throws SQLException {
-		String query = "DELETE FROM feedback WHERE id_feedback ='"+feedback.getId_feedback()+"'";
-        
-        try {
-            con = Connessione.getInstance().getConnection();
-            PreparedStatement pst = con.prepareStatement(query);
-            pst.executeUpdate();
-        } catch (SQLException | ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
+		String query = "DELETE FROM feedback WHERE id_feedback ='" + feedback.getId_feedback() + "'";
+
+		try {
+			con = Connessione.getInstance().getConnection();
+			PreparedStatement pst = con.prepareStatement(query);
+			pst.executeUpdate();
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
 
 	}
 
 	@Override
-	public Feedback doRetrieveByKey(String id_feedback) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public Feedback doRetrieveByKey(int id_feedback) throws SQLException {
+		ResultSet result = null;
+		Feedback fd = new Feedback();
+		String query = "SELECT FROM feedback WHERE id_auto ='" + id_feedback + "'";
+		try {
+			con = Connessione.getInstance().getConnection();
+			PreparedStatement pst = con.prepareStatement(query);
+			result = pst.executeQuery();
+			while (result.next()) {
+				fd.setData(result.getDate("data"));
+				fd.setEmail_utente(result.getString("email_utente"));
+				fd.setId_auto(result.getInt("id_auto"));
+				fd.setTesto(result.getString("testo"));
+				fd.setValutazione(result.getInt("valutazione"));
+				fd.setId_feedback(id_feedback);
+			}
+
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+		return fd;
+
 	}
 
 	@Override
 	public ArrayList<Feedback> doRetrieveAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		ResultSet result = null;
+		Feedback fd = new Feedback();
+		ArrayList<Feedback> allFeedback = new ArrayList<Feedback>();
+		String query = "SELECT * FROM feedback ";
+
+		try {
+			con = Connessione.getInstance().getConnection();
+			PreparedStatement pst = con.prepareStatement(query);
+			result = pst.executeQuery();
+			while(result.next()) {
+				fd.setData(result.getDate("data"));
+				fd.setEmail_utente(result.getString("email_utente"));
+				fd.setId_auto(result.getInt("id_auto"));
+				fd.setTesto(result.getString("testo"));
+				fd.setValutazione(result.getInt("valutazione"));
+				fd.setId_feedback(result.getInt("id_feedback"));
+				
+				allFeedback.add(fd);
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+		return allFeedback;
 	}
 
 }
