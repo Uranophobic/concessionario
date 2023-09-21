6e0ea1d0a17a3e15ad14af82b2e2b3c13689452b
Alessia Crispo <alessia.crispo24@gmail.com>
2023-09-21 10:01:18 +0200

    Concluso macchina implement

--- concessionario/src/main/java/model/MacchinaImplement.java
+++ concessionario/src/main/java/model/MacchinaImplement.java
@@ -48,7 +48,30 @@ public class MacchinaImplement implements MacchinaModel {
 
 	@Override
 	public void doUpdate(Macchina macchina) throws SQLException {
-		// TODO Auto-generated method stub
+		String query = " UPDATE INTO macchina(modello,marca,anno_prod,cilindrata,carburante,colore,stato_veicolo,prezzo,kilometraggio,categoria,posti,tipo_cambio,kW,descrizione)"
+				+ "values (?,?,?,?,?,?,?,?,?,?,?,?,?,?) WHERE  id_auto ='"+ macchina.getId_auto()+"'";
+		try {
+			con = Connessione.getInstance().getConnection();
+			PreparedStatement p = con.prepareStatement(query);
+			p.setString(1, macchina.getModello());
+			p.setString(2, macchina.getMarca());
+			p.setString(3, macchina.getAnno_produzione());
+			p.setInt(4, macchina.getCilindrata());
+			p.setString(5, macchina.getCarburante());
+			p.setString(6, macchina.getColore());
+			p.setString(7, macchina.getStato_veicolo());
+			p.setDouble(8, macchina.getPrezzo());
+			p.setInt(9, macchina.getKilometraggio());
+			p.setString(10, macchina.getCategoria());
+			p.setInt(11, macchina.getPosti());
+			p.setString(12, macchina.getTipo_cambio());
+			p.setInt(13, macchina.getkW());
+			p.setString(14, macchina.getDescrizione());
+			
+			p.executeUpdate();
+		} catch (SQLException | ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+			}
 		
 	}
 
